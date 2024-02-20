0c2e6fa33e7dd363919ccd6931640fe5fe36cb33
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-02-19 20:17:59 -0400

    create controller to search for gyms

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,20 @@
+import { makeSearchGymsUseCase } from "@/use-cases/factories/make-search-gym-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function search(req: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1)
+  })
+
+  const { query, page } = searchGymsQuerySchema.parse(req.params)
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+
+  const { gyms } = await searchGymsUseCase.execute(query, page)
+
+  return reply.send({
+    gyms
+  })
+}
