76a34e1f13db6a653b534ad0c7910e1788eb8cc5
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-02-19 21:11:50 -0400

    add controller that performs checkin validation

---
+++ src/http/controllers/check-ins/validate.ts
@@ -0,0 +1,17 @@
+import { makeValidateCheckInUseCase } from "@/use-cases/factories/make-validate-check-in-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function validate(request: FastifyRequest, reply: FastifyReply) {
+  const validateParamsSchema = z.object({
+    checkInId: z.string().uuid()
+  })
+
+  const { checkInId } = validateParamsSchema.parse(request.params)
+
+  const validateCheckInUseCase = makeValidateCheckInUseCase()
+
+  await validateCheckInUseCase.execute(checkInId)
+
+  return reply.status(204).send()
+}
