27b87cbf6aeb9242eab6dcfc443c5a927180c3a2
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2024-02-19 21:19:16 -0400

    add controllers to their respective routes

---
+++ src/http/routes/checkins.ts
@@ -0,0 +1,15 @@
+import { FastifyInstance } from "fastify";
+import { create } from "../controllers/check-ins/create";
+import { history } from "../controllers/check-ins/history";
+import { JWTVerify } from "../middlewares/jwt-verify";
+import { metrics } from "../controllers/check-ins/metrics";
+import { validate } from "../controllers/check-ins/validate";
+
+export async function checkinsRoutes(app: FastifyInstance) {
+  app.addHook("onRequest", JWTVerify)
+
+  app.post("/gyms/:gymId/checkins", create)
+  app.get("/checkins/history", history)
+  app.get("/checkins/metrics", metrics)
+  app.get("/checkins/:checkInId", validate)
+}
